093bf8ed91a9b7e6ba47ac1e8d587111f49561f3
Kimi <phanlkim@comcast.net>
2021-02-06 23:08:09 -0700

    working on adding task function

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,34 @@
-import { createStore } from 'redux';
-import { defaultState } from '../../server/defaultState'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { defaultState } from '../../server/defaultState';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
-  function reducer(state = defaultState, action) {
-    return state;
-  }
-)
+  combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+      switch(action.type) {
+        case mutations.CREATE_TASK:
+          console.log(action);
+      }
+      return tasks;
+    },
+    comments(comments = defaultState.comments) {
+      return comments;
+    },
+    groups(groups = defaultState.groups) {
+      return groups;
+    },
+    users(users = defaultState.users) {
+      return users;
+    }
+  }),
+  applyMiddleware(createLogger(), sagaMiddleware)
+);
+
+for (let saga in sagas) {
+  sagaMiddleware.run(sagas[saga]);
+}
