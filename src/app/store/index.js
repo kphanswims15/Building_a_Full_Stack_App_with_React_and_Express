bec16744bcb0bd581c952d41a8e1d542914d6c4c
Kimi <phanlkim@comcast.net>
2021-02-08 20:18:41 -0700

    Added create new tasks. For some reason there is an uncaught exception

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -2,17 +2,23 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from '../../server/defaultState';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
-import * as sagas from './sagas.mock';
-import * as mutations from './mutations';
 
 const sagaMiddleware = createSagaMiddleware();
+import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
 
 export const store = createStore(
   combineReducers({
     tasks(tasks = defaultState.tasks, action) {
       switch(action.type) {
         case mutations.CREATE_TASK:
-          console.log(action);
+          return [...tasks, {
+            id: action.taskID,
+            name: "New Task",
+            group: action.groupID,
+            owner: action.ownerID,
+            isComplete: false
+          }]
       }
       return tasks;
     },
